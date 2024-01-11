ce54e3bf9ef286ac334e34ddcd642ada1d0d62ef
Carl <cboettig@gmail.com>
2024-01-11 11:57:00 -0800

    helm launch

---
+++ jupyterhub/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+## use your name for install name and namespace name
+helm upgrade --cleanup-on-fail \
+  --install testjuypterhelm jupyterhub/jupyterhub \
+  --namespace testjupyter \
+  --create-namespace \
+  --version=3.2.1 \
+  --values config.yaml
+
+
