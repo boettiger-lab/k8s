6b339703d79a241728c84926a2e0af46e0b201af
Carl <cboettig@gmail.com>
2024-01-14 21:13:56 -0800

    wip

---
+++ initial-setup.sh
@@ -0,0 +1,61 @@
+# For details, see blog post
+
+
+# Install K3s
+# ============================
+#
+# (Don't really curl ... | sh without inspecting source!)
+# If we are use K8s's default ingress controller (traefik) and use k8s  cert-manager.io for https:
+# curl -sfL https://get.k3s.io | sh -
+
+# Otherwise, to use jupyter's cert manager, or manual caddy, do:
+curl -sfL https://get.k3s.io | sh -s - --disable=traefik 
+
+
+# Install Helm
+# ============================
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Enable GPU + timeslicing
+# ============================
+
+helm upgrade -i nvdp nvdp/nvidia-device-plugin \
+  --namespace nvidia-device-plugin \
+  --create-namespace \
+  --version 0.14.3 \
+  --wait \
+  --values nvidia/nvidia-device-plugin-config.yaml
+
+# identically:
+# bash nvidia/nvidia-device-plugin.sh
+
+
+
+# Launch JupyterHub
+# ==========================
+
+# edit config files appropriately and:
+helm upgrade --cleanup-on-fail \
+  --install testjuypterhelm jupyterhub/jupyterhub \
+  --namespace testjupyter \
+  --create-namespace \
+  --version=3.2.1 \
+  --values public-config.yaml \
+  --values private-config.yaml
+
+# identically:
+# bash jupyterhub/jupyterhub.sh
+
+
+# Additional setup 
+# ==========================
+## if using a cert-manager
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.13.3 \
+  --set installCRDs=true
+
+
+
--- nvidia/nvidia-device-plugin.sh
+++ nvidia/nvidia-device-plugin.sh
@@ -4,8 +4,6 @@ helm upgrade -i nvdp nvdp/nvidia-device-plugin \
   --create-namespace \
   --version 0.14.3 \
   --wait \
-
-
   --values nvidia-device-plugin-config.yaml
 
 
