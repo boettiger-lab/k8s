38cc1d5fb8ba02c3ed7933771f38ebd9f6868cd5
Carl <cboettig@gmail.com>
2024-01-21 16:28:22 -0800

    cirrus

--- nvidia/nvidia-device-plugin.sh
+++ nvidia/nvidia-device-plugin.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+helm repo update
+
 helm upgrade -i nvdp nvdp/nvidia-device-plugin \
   --namespace nvidia-device-plugin \
   --create-namespace \
   --version 0.14.3 \
   --wait \
-  --values nvidia-device-plugin-config.yaml
+  --values nvidia/nvidia-device-plugin-config.yaml
 
 
