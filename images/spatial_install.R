ed4d19af83fc52eaa7ffff20d79166081f8159f2
Carl <cboettig@gmail.com>
2024-02-12 15:55:56 -0800

    update libs

--- images/spatial_install.R
+++ images/spatial_install.R
@@ -2,20 +2,19 @@
 # install R dependencies
 
 ## Ensure spatial packages are installed from source
+Sys.setenv(GITHUB_PAT="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")
+remotes::install_github('r-tmap/tmap', upgrade="never", repos="https://cloud.r-project.org", dep=TRUE)
+
 # We could use renv.lock approach here instead, but will force re-creation of environment from scratch
 # Does not provide a good way to ensure that sf/terra/gdalcubes are installed from source while other packages can be binary
 # Likewise, pak insists on installing old gdal from apt instead of respecting system library source builds
 install.packages("pak")
 pak::pkg_install(c("rstac", "spData", "earthdatalogin", "quarto", "aws.s3", "pak", "duckdbfs", "minioclient"))
-pak::pkg_install("r-tmap/tmap")
-
-#remotes::install_github('r-tmap/tmap', upgrade="never", repos="https://cloud.r-project.org", dep=TRUE)
-
 
 pak::pkg_install("igraph")
 pak::pkg_install("ropensci-review-tools/pkgcheck")
 
-# pak::pkg_install("httpgd")
+pak::pkg_install("httpgd")
 pak::pkg_install(c("IRkernel", "languageserver"))
 IRkernel::installspec()
 
