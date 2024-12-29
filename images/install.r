805592d3af684dc2d170085f4dd3969a867be552
Carl <cboettig@gmail.com>
2024-12-29 15:40:27 -0800

    r-universe

--- images/install.r
+++ images/install.r
@@ -1,13 +1,13 @@
 install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak'))
 
-readr::write_lines('
-  options(repos = list(CRAN = sprintf(
-    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
-    .Platform$pkgType,
-    R.Version()$os,
-    R.Version()$arch
-  )))', 
-  paste0(R.home(), "/etc/Rprofile.site"),
-  append = TRUE)
+readr::write_lines("
+options(repos = c(
+  linux = 'https://jeroen.r-universe.dev/bin/linux/noble/4.4/',
+  sources = 'https://jeroen.r-universe.dev',
+  cran = 'https://cloud.r-project.org'
+))
+", 
+paste0(R.home(), "/etc/Rprofile.site"),
+append = TRUE)
 
 
