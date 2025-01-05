0954bc5941419827edb1ffdda466f0b04947eecc
Carl <cboettig@gmail.com>
2025-01-05 10:35:55 -0800

    extensions

--- images/install.r
+++ images/install.r
@@ -1,2 +1,2 @@
-install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak', 'httpgd', 'languageserver'))
+install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak', 'httpgd', 'languageserver', 'prettycode'))
 
