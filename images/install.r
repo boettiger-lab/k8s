989ad049a6d2154d501080a0b996f4f9862582b1
Carl <cboettig@gmail.com>
2025-01-13 14:31:55 -0800

    update

--- images/install.r
+++ images/install.r
@@ -1,3 +1,3 @@
-install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak', 'httpgd', 'languageserver', 'prettycode', 'protolite'))
+install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak', 'httpgd', 'languageserver', 'prettycode', 'mapboxapi', 'av', 'gifski', 'protolite', 'magick', 'rsvg', 'webp' ))
 
 
