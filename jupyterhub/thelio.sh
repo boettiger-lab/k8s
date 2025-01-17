ed1b043025fc4e6a3d4525841416453d722e93be
Carl <cboettig@gmail.com>
2025-01-16 18:14:03 -0800

    update

--- jupyterhub/thelio.sh
+++ jupyterhub/thelio.sh
@@ -8,7 +8,7 @@ helm upgrade --cleanup-on-fail \
   --install testjuypterhelm jupyterhub/jupyterhub \
   --namespace testjupyter \
   --create-namespace \
-  --version=3.2.1 \
+  --version=4.0.0 \
   --timeout 90m0s \
   --values thelio-config.yaml \
   --values private-config.yaml
