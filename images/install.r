e7900800eee1678691f69121a9a6c86844bea3c2
Carl <cboettig@gmail.com>
2025-04-21 22:13:10 -0700

    here we go

--- images/install.r
+++
@@ -1,3 +0,0 @@
-install.packages(c('tidyverse', 'devtools', 'reticulate', 'nimble', 'duckdbfs', 'arrow', 'gdalcubes', 'rstac', 'terra', 'sf', 'stars', 'quarto', 'mapgl', 'neonstore', 'rfishbase', 'shiny', 'pak', 'httpgd', 'languageserver', 'prettycode', 'mapboxapi', 'av', 'gifski', 'protolite', 'magick', 'rsvg', 'webp', 'V8', 'jsonld'))
-
-
