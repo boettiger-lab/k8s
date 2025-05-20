66c58b98b63200d245efdc7a84af9bc761e680fc
Carl <cboettig@gmail.com>
2025-05-20 07:53:28 -0700

    certs

---
+++ cert-manager/helm.sh
@@ -0,0 +1,11 @@
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+
+helm upgrade \
+ cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --set installCRDs=true
+
+
