14527ca5eac8ebdf58138fd41c60410e14c35602
Carl Boettiger <cboettig@gmail.com>
2025-12-15 12:24:07 -0800

    gpt-oss stuck at 20b

--- vllm/nimbus/up.sh
+++ vllm/nimbus/up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create API key secret if it doesn't exist
-kubectl create secret generic vllm-api-key --from-literal=api-key=$CIRRUS_KEY --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic vllm-api-key --from-literal=api-key=$CIRRUS_KEY --dry-run=client -o yaml | kubectl -n cboettig apply -f -
 
 # Apply Kubernetes manifests
-kubectl apply -f deploy-glm4.5.yaml -n cboettig
+kubectl apply -f deploy-gpt-oss.yaml -n cboettig
 kubectl apply -f service.yaml -n cboettig
 kubectl apply -f ingress.yaml -n cboettig
